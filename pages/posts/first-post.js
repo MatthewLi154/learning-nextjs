559c507912e81d6a7c96ccafee55c67761996e6e
MatthewLi154 <matthewli154@MATTS_IPHONE.localdomain>
2023-03-02 21:19:51 -0800

    added image to first page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function FirstPost() {
   return (
@@ -8,6 +9,12 @@ export default function FirstPost() {
         <Link href="/">Back to home</Link>
         <Link href="/">Also back to home</Link>
       </h2>
+      <Image
+        src="https://i.pinimg.com/564x/ee/97/52/ee9752e8ba0f5db92c8e18bb4c5701a4.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
     </>
   );
 }
